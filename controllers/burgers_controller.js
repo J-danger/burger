208f1c376569cdf50eccf52a9767a9d2569186a1
Stephen Howe <Stevehowe10@gmail.com>
2019-10-17 16:57:40 -0600

    pushing with boolean values now

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,28 +11,35 @@ router.get("/", function(req,res){
 router.get("/burgers", function(req,res){
 	burgers.all(function(data){
 		var newObject = {
-			burgers: data
-		};
-		
+			burgers: data,			
+		};		
 		res.render("index", newObject);
 	});
 });
 
 router.post("/burgers/create", function(req,res){
-	burgers.create(["burger_name"],[req.body.burger_name], function(){                
+	burgers.create(["burger_name", "devoured"],[req.body.burger_name, false], function(){                
 				res.redirect("/burgers");
 			});
 });
-
-router.put("/burgers/update/:id", function(req,res){
+router.put("/burgers/update/:id", function(req, res) {
 	var condition = "id = " + req.params.id;
-	console.log(condition);
-
-	burgers.update({
+  
+	console.log("condition", condition);
+  
+	burgers.update("devoured",
+	  {
 		devoured: req.body.devoured
-	}, condition, function(){
-		res.redirect("/burgers")
-	});
-});
+	  },
+	  condition,
+	  function(result) {
+		if (result.changedRows === 0) {
+		  // If no rows were changed, then the ID must not exist, so 404
+		  return res.status(404).end();
+		}
+		res.status(200).end();  
+	  }
+	);
+  })
 
 module.exports = router;
